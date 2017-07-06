2ea0a9ff6fea62d0ce9aeeed1699f154dc7d81d9
Muhammad Ghazali <muhammadghazali2480@gmail.com>
2017-07-07 00:26:43 +0700

    scaffold the main page with dummy image placeholders
    later this dummy image placeholders should be fetched from themovieDB API

--- app/src/main/java/com/example/popularmovies/popularmovies/MainActivity.java
+++ app/src/main/java/com/example/popularmovies/popularmovies/MainActivity.java
@@ -3,14 +3,29 @@ package com.example.popularmovies.popularmovies;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView mMoviesRecyclerView;
+    private MoviePosterAdapter moviePosterAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mMoviesRecyclerView = (RecyclerView) findViewById(R.id.rv_movie_list);
+
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 2);
+        mMoviesRecyclerView.setLayoutManager(gridLayoutManager);
+        mMoviesRecyclerView.setHasFixedSize(true);
+
+        moviePosterAdapter = new MoviePosterAdapter();
+        mMoviesRecyclerView.setAdapter(moviePosterAdapter);
+
     }
 
     public void goToMovieDetailActivity(View view) {
---
+++ app/src/main/java/com/example/popularmovies/popularmovies/MoviePosterAdapter.java
@@ -0,0 +1,68 @@
+package com.example.popularmovies.popularmovies;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.squareup.picasso.MemoryPolicy;
+import com.squareup.picasso.Picasso;
+
+public class MoviePosterAdapter extends RecyclerView.Adapter<MoviePosterAdapter.MoviePosterAdapterViewHolder> {
+    private String[] mMovieList = {
+            "https://placeimg.com/640/480/nature/grayscale",
+            "https://placeimg.com/640/480/any",
+            "https://placeimg.com/640/480/tech",
+            "https://placeimg.com/640/480/people",
+            "https://placeimg.com/640/480/arch",
+            "https://placeimg.com/640/480/animals",
+            "https://placeimg.com/640/480/animals/sepia",
+            "https://placeimg.com/640/480/any",
+            "https://placeimg.com/640/480/nature/grayscale",
+            "https://placeimg.com/640/480/people/grayscale"
+    };
+
+    private Context mParentContext;
+
+    @Override
+    public void onBindViewHolder(MoviePosterAdapterViewHolder holder, int position) {
+        Picasso
+                .with(holder.mMoviePosterImageView.getContext())
+                .load(mMovieList[position])
+                .into(holder.mMoviePosterImageView);
+    }
+
+    @Override
+    public MoviePosterAdapterViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        mParentContext = parent.getContext();
+        int itemLayoutId = R.layout.movie_poster_item;
+        LayoutInflater inflater = LayoutInflater.from(mParentContext);
+        boolean shouldAttachToParentImmediately = false;
+
+        View view = inflater.inflate(itemLayoutId, parent, shouldAttachToParentImmediately);
+        MoviePosterAdapterViewHolder viewHolder = new MoviePosterAdapterViewHolder(view);
+
+        return viewHolder;
+    }
+
+    @Override
+    public int getItemCount() {
+        if (mMovieList == null) {
+            return 0;
+        }
+
+        return mMovieList.length;
+    }
+
+    public class MoviePosterAdapterViewHolder extends RecyclerView.ViewHolder {
+        public final ImageView mMoviePosterImageView;
+
+        public MoviePosterAdapterViewHolder(View itemView) {
+            super(itemView);
+
+            mMoviePosterImageView = (ImageView) itemView.findViewById(R.id.movie_poster_img);
+        }
+    }
+}
