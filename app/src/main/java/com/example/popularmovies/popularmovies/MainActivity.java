0cc7721bfd2fc1bc8ee252e5dc46f156dfecff48
Muhammad Ghazali <muhammadghazali2480@gmail.com>
2017-07-06 21:37:32 +0700

    add the recyeclerview library to the app
    as the main activity will hold the list of movies, the corresponding layout was changed. The constraint layout are now replaced with FrameLayout

--- app/src/main/java/com/example/popularmovies/popularmovies/MainActivity.java
+++ app/src/main/java/com/example/popularmovies/popularmovies/MainActivity.java
@@ -4,18 +4,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
-    private TextView helloTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        helloTextView = (TextView) findViewById(R.id.hello_world);
     }
 
     public void goToMovieDetailActivity(View view) {
