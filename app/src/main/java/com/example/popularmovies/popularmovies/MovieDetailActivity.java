6e9779992b14f52120da53b70086d7607d512486
Muhammad Ghazali <muhammadghazali2480@gmail.com>
2017-07-27 10:22:44 +0700

    clean up dead code

--- app/src/main/java/com/example/popularmovies/popularmovies/MovieDetailActivity.java
+++ app/src/main/java/com/example/popularmovies/popularmovies/MovieDetailActivity.java
@@ -1,8 +1,8 @@
 package com.example.popularmovies.popularmovies;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -13,7 +13,6 @@ import com.squareup.picasso.Picasso;
 
 import java.io.IOException;
 
-import butterknife.BindDimen;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
