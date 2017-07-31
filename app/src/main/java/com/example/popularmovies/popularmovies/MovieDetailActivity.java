f4a3f22a5a351636623d1036c34f6e2472de6660
Muhammad Ghazali <muhammadghazali2480@gmail.com>
2017-07-31 07:40:27 +0700

    mock up the trailers section
    issue #3

--- app/src/main/java/com/example/popularmovies/popularmovies/MovieDetailActivity.java
+++ app/src/main/java/com/example/popularmovies/popularmovies/MovieDetailActivity.java
@@ -1,11 +1,16 @@
 package com.example.popularmovies.popularmovies;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.codewaves.youtubethumbnailview.ImageLoader;
+import com.codewaves.youtubethumbnailview.ThumbnailLoader;
+import com.codewaves.youtubethumbnailview.ThumbnailView;
 import com.example.popularmovies.popularmovies.models.Movie;
 import com.squareup.moshi.JsonAdapter;
 import com.squareup.moshi.Moshi;
@@ -35,10 +40,24 @@ public class MovieDetailActivity extends AppCompatActivity {
     @BindView(R.id.movie_poster_img)
     ImageView mMoviePosterImageView;
 
+    @BindView(R.id.trailer_1)
+    ThumbnailView mTrailer1;
+
+    @BindView(R.id.trailer_2)
+    ThumbnailView mTrailer2;
+
+    @BindView(R.id.trailer_3)
+    ThumbnailView mTrailer3;
+
+    @BindView(R.id.trailers_list_section)
+    LinearLayout trailersListSection;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_detail);
+
+        ThumbnailLoader.initialize(getApplicationContext());
 
         ButterKnife.bind(this);
 
@@ -68,6 +87,35 @@ public class MovieDetailActivity extends AppCompatActivity {
                 mOverview.setText(movie.getOverview());
                 mAvarageRate.setText(movie.getVoteAverage());
                 mReleaseDate.setText(movie.getReleaseDate());
+
+                mTrailer1.loadThumbnail("https://www.youtube.com/watch?v=iCkYw3cRwLo", new ImageLoader() {
+                    @Override
+                    public Bitmap load(String url) throws IOException {
+                        return Picasso.with(MovieDetailActivity.this).load(url).get();
+                    }
+                });
+
+                mTrailer1.loadThumbnail("https://www.youtube.com/watch?v=Ow78zp30ioE", new ImageLoader() {
+                    @Override
+                    public Bitmap load(String url) throws IOException {
+                        return Picasso.with(MovieDetailActivity.this).load(url).get();
+                    }
+                });
+
+                mTrailer2.loadThumbnail("https://www.youtube.com/watch?v=Sq8vjBg7EWE", new ImageLoader() {
+                    @Override
+                    public Bitmap load(String url) throws IOException {
+                        return Picasso.with(MovieDetailActivity.this).load(url).get();
+                    }
+                });
+
+                mTrailer3.loadThumbnail("https://www.youtube.com/watch?v=MKyp9Tx-NPY", new ImageLoader() {
+                    @Override
+                    public Bitmap load(String url) throws IOException {
+                        return Picasso.with(MovieDetailActivity.this).load(url).get();
+                    }
+                });
+
             }
         }
     }
