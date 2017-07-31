dc3ec7912c844b89e07c06d1612ca590074e4abe
Muhammad Ghazali <muhammadghazali2480@gmail.com>
2017-07-31 13:21:06 +0700

    scaffold the content provider
    issue #5

---
+++ app/src/main/java/com/example/popularmovies/popularmovies/data/MovieContentProvider.java
@@ -0,0 +1,58 @@
+package com.example.popularmovies.popularmovies.data;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+
+public class MovieContentProvider extends ContentProvider {
+
+    private MovieDbHelper mMovieDbHelper;
+
+    @Override
+    public boolean onCreate() {
+        Context context = getContext();
+        mMovieDbHelper = new MovieDbHelper(context);
+        return true;
+    }
+
+
+    @Override
+    public Uri insert(@NonNull Uri uri, ContentValues values) {
+
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+
+    @Override
+    public Cursor query(@NonNull Uri uri, String[] projection, String selection,
+                        String[] selectionArgs, String sortOrder) {
+
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+
+    @Override
+    public int delete(@NonNull Uri uri, String selection, String[] selectionArgs) {
+
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+
+    @Override
+    public int update(@NonNull Uri uri, ContentValues values, String selection,
+                      String[] selectionArgs) {
+
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+
+    @Override
+    public String getType(@NonNull Uri uri) {
+
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+}
