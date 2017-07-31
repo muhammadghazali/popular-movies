c3d03f93ecf0f852b03597ebc95523aeada9f976
Muhammad Ghazali <muhammadghazali2480@gmail.com>
2017-07-31 13:27:17 +0700

    add URIs to contract
    for now the MainActivity would be responsible to hold the movies when the app is launched
    
    issue #5

--- app/src/main/java/com/example/popularmovies/popularmovies/data/MovieContract.java
+++ app/src/main/java/com/example/popularmovies/popularmovies/data/MovieContract.java
@@ -1,9 +1,21 @@
 package com.example.popularmovies.popularmovies.data;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 public class MovieContract {
+
+    public static final String AUTHORITY = "com.example.popularmovies.popularmovies";
+
+    // The base content URI = "content://" + <authority>
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + AUTHORITY);
+
+    public static final String PATH_MOVIES = "movies";
+
     public static final class MovieEntry implements BaseColumns {
+        public static final Uri CONTENT_URI =
+                BASE_CONTENT_URI.buildUpon().appendPath(PATH_MOVIES).build();
+
         public static final String TABLE_NAME = "movies";
 
         public static final String COLUMN_POSTER = "poster";
