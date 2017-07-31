8a389fb0da8291f6fd0dff400b06231f018e1fd8
Muhammad Ghazali <muhammadghazali2480@gmail.com>
2017-07-31 13:34:21 +0700

    scaffold the URI matchers
    issue #5

--- app/src/main/java/com/example/popularmovies/popularmovies/data/MovieContentProvider.java
+++ app/src/main/java/com/example/popularmovies/popularmovies/data/MovieContentProvider.java
@@ -3,6 +3,7 @@ package com.example.popularmovies.popularmovies.data;
 import android.content.ContentProvider;
 import android.content.ContentValues;
 import android.content.Context;
+import android.content.UriMatcher;
 import android.database.Cursor;
 import android.net.Uri;
 import android.support.annotation.NonNull;
@@ -10,6 +11,24 @@ import android.support.annotation.NonNull;
 public class MovieContentProvider extends ContentProvider {
 
     private MovieDbHelper mMovieDbHelper;
+
+    public static final int MOVIES = 100;
+    public static final int MOVIE_WITH_ID = 101;
+
+    private static final UriMatcher sUriMatcher = buildUriMatcher();
+
+    /**
+     * Initialize a new matcher object without any matches,
+     * then use .addURI(String authority, String path, int match) to add matches
+     */
+    public static UriMatcher buildUriMatcher() {
+        UriMatcher uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+        uriMatcher.addURI(MovieContract.AUTHORITY, MovieContract.PATH_MOVIES, MOVIES);
+        uriMatcher.addURI(MovieContract.AUTHORITY, MovieContract.PATH_MOVIES + "/#", MOVIE_WITH_ID);
+
+        return uriMatcher;
+    }
 
     @Override
     public boolean onCreate() {
